e23e38ee8f815f1d22aba6ce541034950d332f7b
Mike737377 <no@email.please>
2014-06-11 14:21:01 +1000

    Create Html Modal helper

---
+++ src/Exceptional.Web/Extensions/HtmlExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Exceptional.Web.HtmlExtensions
+{
+    public static class HtmlExtensions
+    {
+        private static readonly string ModalFormat = @"<div class=""modal fade"" id=""{0}"" tabindex=""-1"" role=""dialog"" aria-hidden=""true""><div class=""modal-dialog"">{1}<div class=""modal-content""></div></div></div>";
+
+        public static MvcHtmlString Modal<TModel>(this HtmlHelper<TModel> htmlHelper, string modalId, string partialViewName, TModel model)
+        {
+            return new MvcHtmlString(string.Format(ModalFormat, modalId, htmlHelper.Partial(partialViewName, model)));
+        }
+
+        public static MvcHtmlString Modal(this HtmlHelper htmlHelper, string modalId, string partialViewName)
+        {
+            return new MvcHtmlString(string.Format(ModalFormat, modalId, htmlHelper.Partial(partialViewName)));
+        }
+    }
+}
